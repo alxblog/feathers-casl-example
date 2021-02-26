75722aec75c8e97aa9f19ff51bc9a3232b3f5008
Alexandre <3979539+alxblog@users.noreply.github.com>
2021-02-26 16:46:54 +0100

    Remove availableFields()

--- src/services/tasks/tasks.hooks.js
+++ src/services/tasks/tasks.hooks.js
@@ -1,11 +1,6 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { authorize } = require('feathers-casl').hooks;
 
-const availableFields = (context) => {
-  const { rawAttributes } = context.service.Model;
-  return Object.keys(rawAttributes);
-}
-
 module.exports = {
   before: {
     all: [authenticate('jwt')],
@@ -13,37 +8,31 @@ module.exports = {
       (context) => (console.log('context', context)),
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ],
     get: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ],
     create: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ],
     update: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ],
     patch: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ],
     remove: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }) // make sure this hook runs always last
     ]
   },
@@ -52,7 +41,6 @@ module.exports = {
     all: [
       authorize({
         checkAbilityForInternal: true,
-        availableFields
       }), // make sure this hook runs always first
     ],
     find: [],
